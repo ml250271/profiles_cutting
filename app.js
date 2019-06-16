068418ecb870926457c37b14a7c643f1e08fa1bb
ml250271 <lukicmilica@hotmail.com>
2019-06-16 13:45:47 +0200

    add function that makes array of profiles

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 // fake data:
 const profileSize = 10;
-const piecesToCut = [7, 6, 5, 4, 3, 2, 2];
+const piecesToCut = [7, 6, 5, 4, 3, 2, 2, 9, 9, 9];
 
-function minNumOfProfiles(pieces = [], size) {
+function countMinNumOfProfiles(pieces = [], size) {
     let sum = pieces.reduce( (sum, val) => { return sum + val }, 0);
     return Math.ceil(sum / size);
     
 }
 
-console.log(`Minimun number of profiles needed is: ${minNumOfProfiles(piecesToCut, profileSize)}`);
+const numOfProfiles = countMinNumOfProfiles(piecesToCut, profileSize);
+console.log(`Minimun number of profiles needed is: ${numOfProfiles}`);
 
+
+// array of (unused) profiles. ex: [0,0,0,0,0] - 5 unused pr
+
+function makeProfilesArr(num) {
+    let profilesArr = [];
+    for (let i=0; i <= num; i++) {
+        profilesArr.push(0);
+        }
+            return profilesArr;        
+};
+let profilesArr = makeProfilesArr(numOfProfiles);
+console.log(`Profiles array: ${profilesArr}`);
 
