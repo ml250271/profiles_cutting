03d7017a248056888038b77f352663b8bf2a5867
ml250271 <lukicmilica@hotmail.com>
2019-06-16 17:30:08 +0200

    add mainCounting function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // fake data:
 const profileSize = 10;
-const piecesToCut = [7, 6, 5, 4, 3, 2, 2, 9, 9, 9];
+const piecesToCut = [7, 6, 5, 4, 3, 2, 2, 9, 2, 4, 9, 8, 6];
 
 function countMinNumOfProfiles(pieces = [], size) {
     let sum = pieces.reduce( (sum, val) => { return sum + val }, 0);
@@ -8,19 +8,42 @@ function countMinNumOfProfiles(pieces = [], size) {
     
 }
 
-const numOfProfiles = countMinNumOfProfiles(piecesToCut, profileSize);
+let numOfProfiles = countMinNumOfProfiles(piecesToCut, profileSize);
 console.log(`Minimun number of profiles needed is: ${numOfProfiles}`);
 
 
 // array of (unused) profiles. ex: [0,0,0,0,0] - 5 unused pr
 
-function makeProfilesArr(num) {
-    let profilesArr = [];
-    for (let i=0; i <= num; i++) {
-        profilesArr.push(0);
-        }
-            return profilesArr;        
-};
-let profilesArr = makeProfilesArr(numOfProfiles);
-console.log(`Profiles array: ${profilesArr}`);
+// function makeProfilesArr(num) {
+//     let profilesArr = [];
+//     for (let i=0; i <= num; i++) {
+//         profilesArr.push(0);
+//         }
+//             return profilesArr;        
+// };
+// let profilesArr = makeProfilesArr(numOfProfiles);
+// console.log(`Profiles array: ${profilesArr}`);
 
+
+
+function mainCounting(pieces) {
+    let resultArray = [];
+    pieces.sort((a, b) => b - a);  // [9,7,6,5,4,3,2,2]; 
+    console.log('sorted pieces ' + pieces);
+    
+
+    pieces.forEach(piece => {
+        const availablePlace = resultArray.findIndex((subarray)=> {
+            return (profileSize - subarray.reduce((a, b) => a+b) >= piece) 
+        });  
+        
+        if (availablePlace < 0) {
+            resultArray.push([piece])
+        }else {
+            resultArray[availablePlace].push(piece);
+        } 
+        console.table('rarr', resultArray);
+    }) 
+};
+
+mainCounting(piecesToCut);
